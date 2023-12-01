134e277a66f7c1a7c4ca5a9ea0f80913a3293021
markel <m.ryabtsev@outlook.com>
2023-12-01 16:14:17 +0600

    added 9 lab

---
+++ lab_work_9/ColorMixer.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace lab_work_9;
+
+public partial class ColorMixer : Window
+{
+    public ColorMixer()
+    {
+        InitializeComponent();
+        UpdateColor();
+    }
+
+    private void UpdateColor()
+    {
+        try
+        {
+            var rColorValue = RColorControlCheckBox.IsChecked == true ? int.Parse(RValueTextBox.Text) : 0;
+            var gColorValue = GColorControlCheckBox.IsChecked == true ? int.Parse(GValueTextBox.Text) : 0;
+            var bColorValue = BColorControlCheckBox.IsChecked == true ? int.Parse(BValueTextBox.Text) : 0;
+
+            var newColor = Color.FromRgb(
+                r: (byte)rColorValue,
+                g: (byte)gColorValue,
+                b: (byte)bColorValue
+            );
+            ColorPreviewStackPanel.Background = new SolidColorBrush(newColor);
+        }
+        catch (Exception e)
+        {
+            ColorPreviewStackPanel.Background = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+    }
+
+    private void RValueSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        ((Slider)sender).SelectionEnd = e.NewValue;
+        RValueTextBox.Text = ((int)e.NewValue).ToString();
+        UpdateColor();
+    }
+
+    private void GValueSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        ((Slider)sender).SelectionEnd = e.NewValue;
+        GValueTextBox.Text = ((int)e.NewValue).ToString();
+        UpdateColor();
+    }
+
+    private void BValueSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        ((Slider)sender).SelectionEnd = e.NewValue;
+        BValueTextBox.Text = ((int)e.NewValue).ToString();
+        UpdateColor();
+    }
+
+    private void RColorControlCheckBox_OnChecked(object sender, RoutedEventArgs e)
+    {
+        UpdateColor();
+    }
+
+    private void RColorControlCheckBox_OnUnchecked(object sender, RoutedEventArgs e)
+    {
+        UpdateColor();
+    }
+
+    private void GColorControlCheckBox_OnChecked(object sender, RoutedEventArgs e)
+    {
+        UpdateColor();
+    }
+
+    private void GColorControlCheckBox_OnUnchecked(object sender, RoutedEventArgs e)
+    {
+        UpdateColor();
+    }
+
+    private void BColorControlCheckBox_OnChecked(object sender, RoutedEventArgs e)
+    {
+        UpdateColor();
+    }
+
+    private void BColorControlCheckBox_OnUnchecked(object sender, RoutedEventArgs e)
+    {
+        UpdateColor();
+    }
+
+    private void RValueTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            RValueSlider.Value = double.Parse(((TextBox)sender).Text);
+            UpdateColor();
+        }
+        catch (Exception exception)
+        {
+        }
+    }
+
+    private void GValueTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            GValueSlider.Value = double.Parse(((TextBox)sender).Text);
+            UpdateColor();
+        }
+        catch (Exception exception)
+        {
+        }
+    }
+
+    private void BValueTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            BValueSlider.Value = double.Parse(((TextBox)sender).Text);
+            UpdateColor();
+        }
+        catch (Exception exception)
+        {
+        }
+    }
+}
---
+++ lab_work_9/model/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using lab_work_7_func_module.functions;
+
+namespace lab_work_9.model;
+
+public class Result
+{
+    public double X { get; private set; }
+    public int N { get; private set; }
+    
+    public double ApproximateFunc { get; private set; }
+    public double OriginalFunc { get; private set; }
+    
+    public string ApproximateFuncName { get; private set; }
+    public string OriginalFuncName { get; private set; }
+
+    public Result(double x, int n, Function func)
+    {
+        this.X = x;
+        this.N = n;
+        this.ApproximateFunc = func.FindApproximateFunc(x, n);
+        this.OriginalFunc = func.FindOriginalFunc(x);
+        this.ApproximateFuncName = func.ApproximateFunc;
+        this.OriginalFuncName = func.OriginalFunc;
+    }
+}
